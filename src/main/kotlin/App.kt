f237f886abce860ff8c81771dee346e74cf1d589
Lukas Georgieff <lgeorgieff@outlook.com>
2020-12-27 16:48:12 +0100

    src: Implement component for VideoPlayer

--- src/main/kotlin/App.kt
+++ src/main/kotlin/App.kt
@@ -1,45 +1,57 @@
-import kotlinx.css.*
-import kotlinx.html.video
 import react.*
 import react.dom.*
-import styled.css
-import styled.styledDiv
 
 external interface AppState : RState {
     var currentVideo: Video?
+    var unwatchedVideos: List<Video>
+    var watchedVideos: List<Video>
 }
 
 class App : RComponent<RProps, AppState>() {
+    override fun AppState.init() {
+        unwatchedVideos = listOf(
+            Video(1, "Building and breaking things", "John Doe", "https://youtu.be/PsaFVLr8t4E"),
+            Video(2, "The development process", "Jane Smith", "https://youtu.be/PsaFVLr8t4E"),
+            Video(3, "The Web 7.0", "Matt Miller", "https://youtu.be/PsaFVLr8t4E")
+        )
+        watchedVideos = listOf(
+            Video(4, "Mouseless development", "Tom Jerry", "https://youtu.be/PsaFVLr8t4E")
+        )
+    }
+
     override fun RBuilder.render() {
         h1 { +"KotlinConf Explorer" }
         div {
             h3 { +"Videos to watch" }
             videoList {
-                videos = UNWATCHED_VIDEOS
+                videos = state.unwatchedVideos
                 selectedVideo = state.currentVideo
                 onSelectVideo = { video -> setState { currentVideo = video } }
             }
 
             h3 { +"Videos watched" }
             videoList {
-                videos = WATCHED_VIDEOS
+                videos = state.watchedVideos
                 selectedVideo = state.currentVideo
                 onSelectVideo = { video -> setState { currentVideo = video } }
             }
-
         }
-        styledDiv {
-            css {
-                position = Position.absolute
-                top = 10.px
-                right = 10.px
-            }
-            h3 {
-                +"John Doe: Building and breaking things"
-            }
-            img {
-                attrs {
-                    src = "https://via.placeholder.com/640x360.png?text=Video+Player+Placeholder"
+        state.currentVideo?.let { currentVideo ->
+            videoPlayer {
+                video = currentVideo
+                unwatchedVideo = currentVideo in state.unwatchedVideos
+                onWatchedButtonPressed = {
+                    if (video in state.unwatchedVideos) {
+                        setState {
+                            unwatchedVideos -= video
+                            watchedVideos += video
+                        }
+                    } else {
+                        setState {
+                            watchedVideos -= video
+                            unwatchedVideos += video
+                        }
+                    }
                 }
             }
         }
--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,15 +1,6 @@
 import kotlinx.browser.document
 import react.dom.*
 
-val UNWATCHED_VIDEOS = listOf(
-    Video(1, "Building and breaking things", "John Doe", "https://youtu.be/PsaFVLr8t4E"),
-    Video(2, "The development process", "Jane Smith", "https://youtu.be/PsaFVLr8t4E"),
-    Video(3, "The Web 7.0", "Matt Miller", "https://youtu.be/PsaFVLr8t4E")
-)
-val WATCHED_VIDEOS = listOf(
-    Video(4, "Mouseless development", "Tom Jerry", "https://youtu.be/PsaFVLr8t4E")
-)
-
 fun main() {
     render(document.getElementById("root")) {
         child(App::class) {}
